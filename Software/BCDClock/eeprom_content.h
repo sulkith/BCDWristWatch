649b0b0fe2411bb291215dc522b93b4c56d66fae
sulkith <sulkith@gmail.com>
2018-12-21 23:43:04 +0100

    final measurement and eeprom content for PCB2 --> my current Prototype

--- Software/BCDClock/eeprom_content.h
+++ Software/BCDClock/eeprom_content.h
@@ -5,4 +5,5 @@ typedef struct{
 	int8_t everyMonth;
 } correction_t;
 
-correction_t EEMEM correction_EEP={0,2,0,0};
+//correction_t EEMEM correction_EEP={0,2,0,0};
+correction_t EEMEM correction_EEP={0,1,11,13}; //remaining Difference per Day: 0.137259516460902 PCB2
