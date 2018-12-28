4de203cde915f903283e6f602c50384767103aff
sulkith <sulkith@gmail.com>
2018-12-28 17:23:49 +0100

    Added better EEPROM Values for PCB2

--- Software/BCDClock/eeprom_content.h
+++ Software/BCDClock/eeprom_content.h
@@ -6,4 +6,4 @@ typedef struct{
 } correction_t;
 
 //correction_t EEMEM correction_EEP={0,2,0,0};
-correction_t EEMEM correction_EEP={0,1,11,13}; //remaining Difference per Day: 0.137259516460902 PCB2
+correction_t EEMEM correction_EEP={0,2,0,13}; 
