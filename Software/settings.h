5fd37f406a8f0c91ca1f0c2f3fc415b82eea2197
sulkith <sulkith@posteo.de>
2020-05-03 19:15:52 +0200

    cleaned up Headers

--- Software/settings.h
+++ Software/settings.h
@@ -1,7 +1,7 @@
 #ifndef __INCLUDE_SETTINGS_H__
 #define __INCLUDE_SETTINGS_H__
-#include "constants.h"
 
+/*
 const uint8_t shifting[] =
 {
 	DISP_0,
@@ -23,7 +23,7 @@ const uint16_t ontime_short=5000;
 const uint16_t ontime_long=30000;
 //const uint16_t Button_debounceTime=30; //moved to  TwoButtonHAL
 const uint8_t LED_Brightness = 240;
-
+*/
 //SW V2
 //incement this on every change to the EEPROM so the Values gets initialized again
 const uint16_t eepVersion = 1;
@@ -34,5 +34,8 @@ const uint8_t iniCorrEveryDay = 14;
 const uint8_t iniCorrEveryMonth = 0;
 const uint8_t iniActivatedFeatures = 0;
 const uint16_t iniTempOffset = 345;
+
+const uint8_t development = 255;
+
 
 #endif
